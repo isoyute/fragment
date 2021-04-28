73ccafe158a1dbcacb4e07117f7cc1980ee68d3d
isoyute <tudor.s.lupu@gmail.com>
2021-04-27 22:05:25 -0400

    added packages necessary for home page

--- client/src/components/Pages/Home.js
+++ client/src/components/Pages/Home.js
@@ -1,67 +1,85 @@
 import React, { useState } from 'react';
-import { Text, Card, Button} from '@geist-ui/react';
+import { Text, Card, Button } from '@geist-ui/react';
 import { Link as OutLink } from '@geist-ui/react';
 import styles from '../../styles/Home.module.scss';
-import { merge, fadeIn, slideInLeft, slideInRight, zoomIn} from 'react-animations'
+import {
+	merge,
+	fadeIn,
+	slideInLeft,
+	slideInRight,
+	zoomIn,
+} from 'react-animations';
 import styled, { keyframes } from 'styled-components';
 import ParticlesBg from 'particles-bg';
 import axios from 'axios';
 
-const fadeLeft = merge(slideInLeft,fadeIn);
-const fadeRight = merge(slideInRight,fadeIn);
-const fadeUp = merge(zoomIn, fadeIn)
-const SlideInLeft = styled.div`animation: 2s ${keyframes`${fadeLeft}`}`;
-const SlideInRight = styled.div`animation: 2s ${keyframes`${fadeRight}`}`;
-const SlideInUp = styled.div`animation: 2s ${keyframes`${fadeUp}`}`;
+const fadeLeft = merge(slideInLeft, fadeIn);
+const fadeRight = merge(slideInRight, fadeIn);
+const fadeUp = merge(zoomIn, fadeIn);
+const SlideInLeft = styled.div`
+	animation: 2s ${keyframes`${fadeLeft}`};
+`;
+const SlideInRight = styled.div`
+	animation: 2s ${keyframes`${fadeRight}`};
+`;
+const SlideInUp = styled.div`
+	animation: 2s ${keyframes`${fadeUp}`};
+`;
 
 const Home = () => {
 	const [isButtonLoading, setIsButtonLoading] = useState(false);
-	const [repos,setRepos] = useState([]);
+	const [repos, setRepos] = useState([]);
 
 	return (
 		<>
-			<ParticlesBg type="cobweb" bg={{position: "absolute",zIndex: 0,top: 0, left:0}}></ParticlesBg>
+			<ParticlesBg
+				type='cobweb'
+				bg={{ position: 'absolute', zIndex: 0, top: 0, left: 0 }}
+			></ParticlesBg>
 			<div className={styles.home}>
 				<SlideInLeft>
-					<Text h1 className={styles.logo}>Welcome to</Text>
+					<Text h1 className={styles.logo}>
+						Welcome to
+					</Text>
 				</SlideInLeft>
 				<SlideInRight>
-					<Text h1 size={80} className={styles.logo}><i>fragment</i></Text>
+					<Text h1 size={80} className={styles.logo}>
+						<i>fragment</i>
+					</Text>
 				</SlideInRight>
 				<SlideInUp>
+					<GetGitHub repos={repos} setRepos={setRepos} />
 					<OutLink
 						color
 						href={`${process.env.REACT_APP_SERVER_URL}/auth/github/login`}
 						onClick={() => setIsButtonLoading(true)}
 					>
-						<Button loading={isButtonLoading} type="success" size="large">Login/Signup with GitHub</Button>
+						<Button loading={isButtonLoading} type='success' size='large'>
+							Login/Signup with GitHub
+						</Button>
 					</OutLink>
-					<GetGitHub repos={repos} setRepos={setRepos}/>
 				</SlideInUp>
 			</div>
 		</>
 	);
 };
 
-function GetGitHub({repos,setRepos}) {
-	if(repos.length ===0){
+const GetGitHub = ({ repos, setRepos }) => {
+	if (repos.length === 0) {
 		axios
-			.get(`https://api.github.com/repos/igranic8720/GD-FinalProj`)
-			.then((response) => {
-				console.log(response);
-				return response;
+			.get(`https://api.github.com/repos/isoyute/fragment`)
+			.then(response => response)
+			.then(data => {
+				setRepos(data.data);
 			})
-			.then((data)=>{
-				setRepos(data.data)
-			})
-			.catch((error)=>{
+			.catch(error => {
 				console.log(error);
-			});;
+			});
 	}
 	return (
 		<>
-			<Card type="secondary" shadow className={styles.githubcards}>
-				<Text h2>{repos.name}</Text>
+			<Card type='secondary' shadow className={styles.githubcards}>
+				{/* <Text h2>{repos.name}</Text> */}
 				<Text h3>Description</Text>
 				<Text h4>{repos.description}</Text>
 				<Text h3>Language</Text>
